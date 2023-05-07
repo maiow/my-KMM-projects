f7c9b41d8d3802f71737ba0f4f7fcaac0e032582
@maiow <emmaria@yandex.ru>
2023-05-07 21:45:35 +0300

    all dependencies are ok, works well

---
+++ KMM-KtorPlayground/shared/src/commonMain/kotlin/com/mivanovskaya/ktorkmm/Greeting.kt
@@ -0,0 +1,32 @@
+package com.mivanovskaya.ktorkmm
+
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    @Throws(Exception::class)
+    suspend fun greet(): String {
+        val rockets: List<RocketLaunch> =
+            httpClient.get("https://api.spacexdata.com/v4/launches").body()
+        val lastSuccessLaunch = rockets.last { it.launchSuccess == true }
+        return "Guess what it is! > ${platform.name.reversed()}!" +
+                "\nThe last successful launch was ${lastSuccessLaunch.launchDateUTC} 🚀"
+    }
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+}
