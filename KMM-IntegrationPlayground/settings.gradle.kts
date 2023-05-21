4db346a63f575b7b86f7e48a65d0a6eb5a04f282
@maiow <emmaria@yandex.ru>
2023-05-22 01:30:48 +0300

    shared module created and common files moved

---
+++ KMM-IntegrationPlayground/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Simple login"
+include("app")
+include(":shared")
+include(":shared")
